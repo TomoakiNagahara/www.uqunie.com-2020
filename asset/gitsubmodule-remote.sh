858c479a08d0e75c40dfb7dfd0d805b4a6368bd9
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-07-21 17:07:23 +0900

    New: asset/gitsubmodule-remote.sh

---
+++ asset/gitsubmodule-remote.sh
@@ -0,0 +1,70 @@
+
+#  webpack
+cd webpack
+git remote set-url origin https://github.com/TomoakiNagahara/op-module-webpack.git
+git remote add     grand  https://github.com/onepiece-framework/module-webpack.git
+
+#  asset/core
+cd ../asset/core
+git remote set-url origin https://github.com/TomoakiNagahara/op-core.git
+git remote add     grand  https://github.com/onepiece-framework/core.git
+
+#  asset/testcase
+cd ../testcase
+git remote set-url origin https://github.com/TomoakiNagahara/op-module-testcase.git
+git remote add     grand  https://github.com/onepiece-framework/module-testcase.git
+
+#  asset/reference
+cd ../reference
+git remote set-url origin https://github.com/TomoakiNagahara/op-module-reference.git
+git remote add     grand  https://github.com/onepiece-framework/module-reference.git
+
+#  asset/develop
+cd ../develop
+git remote set-url origin https://github.com/TomoakiNagahara/op-module-develop.git
+git remote add     grand  https://github.com/onepiece-framework/module-develop.git
+
+#  asset/unit/api
+cd ../unit/api
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-api.git
+git remote add     grand  https://github.com/onepiece-framework/unit-api.git
+
+#  asset/unit/app
+cd ../app
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-app.git
+git remote add     grand  https://github.com/onepiece-framework/unit-app.git
+
+#  asset/unit/curl
+cd ../curl
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-curl.git
+git remote add     grand  https://github.com/onepiece-framework/unit-curl.git
+
+#  asset/unit/dump
+cd ../dump
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-dump.git
+git remote add     grand  https://github.com/onepiece-framework/unit-dump.git
+
+#  asset/unit/layout
+cd ../layout
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-layout.git
+git remote add     grand  https://github.com/onepiece-framework/unit-layout.git
+
+#  asset/unit/notice
+cd ../notice
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-notice.git
+git remote add     grand  https://github.com/onepiece-framework/unit-notice.git
+
+#  asset/unit/router
+cd ../router
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-router.git
+git remote add     grand  https://github.com/onepiece-framework/unit-router.git
+
+#  asset/unit/template
+cd ../template
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-template.git
+git remote add     grand  https://github.com/onepiece-framework/unit-template.git
+
+#  asset/unit/webpack
+cd ../webpack
+git remote set-url origin https://github.com/TomoakiNagahara/op-unit-webpack.git
+git remote add     grand  https://github.com/onepiece-framework/unit-webpack.git

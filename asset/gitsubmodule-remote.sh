8c56f77f86bc36da321e41f69b3c20d022abbcdd
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-08-08 17:15:43 +0900

    Fix: asset/gitsubmodule-remote.sh | Copy from uqunie.com.

--- asset/gitsubmodule-remote.sh
+++ asset/gitsubmodule-remote.sh
@@ -1,96 +1,82 @@
 
 #  Rename remote name
-# git submodule foreach git remote rename origin grand
+git submodule foreach git remote rename origin grand
 
 #  Remove remote name
-# git submodule foreach git remote rm github
+git submodule foreach git remote rm github
+
+#  skeleton
+git remote add     github https://github.com/TomoakiNagahara/www.uqunie.com-2020.git
+git remote add     origin repo:~/repo/op/www/uqunie/com/2020.git
 
 #  webpack
 cd webpack
-git remote set-url origin repo:~/repo/op/module/webpack.git
+git remote add     origin repo:~/repo/op/module/webpack.git
 git remote add     github https://github.com/TomoakiNagahara/op-module-webpack.git
-git remote add     grand  https://github.com/onepiece-framework/module-webpack.git
 
 #  asset/core
 cd ../asset/core
-git remote set-url origin repo:~/repo/op/core.git
+git remote add     origin repo:~/repo/op/core.git
 git remote add     github https://github.com/TomoakiNagahara/op-core.git
-git remote add     grand  https://github.com/onepiece-framework/core.git
 
 #  asset/testcase
 cd ../testcase
-git remote set-url origin repo:~/repo/op/module/testcase.git
+git remote add     origin repo:~/repo/op/module/testcase.git
 git remote add     github https://github.com/TomoakiNagahara/op-module-testcase.git
-git remote add     grand  https://github.com/onepiece-framework/module-testcase.git
 
 #  asset/reference
 cd ../reference
-git remote set-url origin repo:~/repo/op/module/reference.git
+git remote add     origin repo:~/repo/op/module/reference.git
 git remote add     github https://github.com/TomoakiNagahara/op-module-reference.git
-git remote add     grand  https://github.com/onepiece-framework/module-reference.git
 
 #  asset/develop
 cd ../develop
-git remote set-url origin repo:~/repo/op/module/develop.git
+git remote add     origin repo:~/repo/op/module/develop.git
 git remote add     github https://github.com/TomoakiNagahara/op-module-develop.git
-git remote add     grand  https://github.com/onepiece-framework/module-develop.git
-
-#  asset/unit/api
-cd ../unit/api
-git remote set-url origin repo:~/repo/op/unit/api.git
-git remote add     github https://github.com/TomoakiNagahara/op-unit-api.git
-git remote add     grand  https://github.com/onepiece-framework/unit-api.git
 
 #  asset/unit/app
-cd ../app
-git remote set-url origin repo:~/repo/op/unit/app.git
+cd ../unit/app
+git remote add     origin repo:~/repo/op/unit/app.git
 git remote add     github https://github.com/TomoakiNagahara/op-unit-app.git
-git remote add     grand  https://github.com/onepiece-framework/unit-app.git
-
-#  asset/unit/curl
-cd ../curl
-git remote set-url origin repo:~/repo/op/unit/curl.git
-git remote add     github https://github.com/TomoakiNagahara/op-unit-curl.git
-git remote add     grand  https://github.com/onepiece-framework/unit-curl.git
 
 #  asset/unit/dump
 cd ../dump
-git remote set-url origin repo:~/repo/op/unit/dump.git
+git remote add     origin repo:~/repo/op/unit/dump.git
 git remote add     github https://github.com/TomoakiNagahara/op-unit-dump.git
-git remote add     grand  https://github.com/onepiece-framework/unit-dump.git
 
 #  asset/unit/layout
 cd ../layout
-git remote set-url origin repo:~/repo/op/unit/layout.git
+git remote add     origin repo:~/repo/op/unit/layout.git
 git remote add     github https://github.com/TomoakiNagahara/op-unit-layout.git
-git remote add     grand  https://github.com/onepiece-framework/unit-layout.git
 
 #  asset/unit/notice
 cd ../notice
-git remote set-url origin repo:~/repo/op/unit/notice.git
+git remote add     origin repo:~/repo/op/unit/notice.git
 git remote add     github https://github.com/TomoakiNagahara/op-unit-notice.git
-git remote add     grand  https://github.com/onepiece-framework/unit-notice.git
 
 #  asset/unit/router
 cd ../router
-git remote set-url origin repo:~/repo/op/unit/router.git
+git remote add     origin repo:~/repo/op/unit/router.git
 git remote add     github https://github.com/TomoakiNagahara/op-unit-router.git
-git remote add     grand  https://github.com/onepiece-framework/unit-router.git
 
 #  asset/unit/template
 cd ../template
-git remote set-url origin repo:~/repo/op/unit/template.git
+git remote add     origin repo:~/repo/op/unit/template.git
 git remote add     github https://github.com/TomoakiNagahara/op-unit-template.git
-git remote add     grand  https://github.com/onepiece-framework/unit-template.git
 
 #  asset/unit/webpack
 cd ../webpack
-git remote set-url origin repo:~/repo/op/unit/webpack.git
+git remote add     origin repo:~/repo/op/unit/webpack.git
 git remote add     github https://github.com/TomoakiNagahara/op-unit-webpack.git
-git remote add     grand  https://github.com/onepiece-framework/unit-webpack.git
 
 #  asset/layout/white
 cd ../../layout/white
-git remote set-url origin repo:~/repo/op/layout/white.git
+git remote add     origin repo:~/repo/op/layout/white.git
 git remote add     github https://github.com/TomoakiNagahara/op-layout-white.git
-git remote add     grand  https://github.com/onepiece-framework/layout-white.git
+
+#  Fetch
+git submodule foreach git fetch github
+git submodule foreach git fetch origin
+
+# Change branch
+git submodule foreach git checkout 2020
